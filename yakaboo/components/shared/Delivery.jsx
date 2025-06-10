964f28b500dbdbb96bd2df071f488118a2238691
Dimaaap <procdima49@gmail.com>
2025-06-10 21:32:23 +0300

    small chanages

--- yakaboo/components/shared/Delivery.jsx
+++ yakaboo/components/shared/Delivery.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import Image from "next/image"
+import React from 'react';
+import Image from 'next/image';
 
-export const Delivery = ({ city="Київ" }) => {
+export const Delivery = ({ city = 'Київ' }) => {
   return (
     <div className="container-info__delivery delivery">
-        <Image src="/icons/location.svg" alt="20" height="20" width="20" />
-        <div className="delivery__text">
-            <p className="delivery__city-title">
-                { city }
-            </p>
-            <p className="delivery__city-description">
-                Нижче наведені умови доставки
-            </p>
-        </div>
+      <Image src="/icons/location.svg" alt="20" height="20" width="20" />
+      <div className="delivery__text">
+        <p className="delivery__city-title">{city}</p>
+        <p className="delivery__city-description">
+          Нижче наведені умови доставки
+        </p>
+      </div>
     </div>
-  )
-}
-
+  );
+};
