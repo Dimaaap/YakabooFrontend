eac309c69423632c92e4031337a7da9d2f083f79
Dimaaap <procdima49@gmail.com>
2025-06-17 17:58:17 +0300

    Add fetching countries and countries select

--- yakaboo/components/modals/DeliveryInfoModal.jsx
+++ yakaboo/components/modals/DeliveryInfoModal.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { handleBackdropClick } from '../../services';
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
+
+import { fetchData, handleBackdropClick } from '../../services';
 import { useDeliveryModalStore } from '../../states';
 import { ModalCloseBtn } from '../shared';
+import Endpoints from '../../endpoints';
 
 export const DeliveryInfoModal = () => {
   const { setIsDeliveryModalOpen } = useDeliveryModalStore();
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    fetchData(Endpoints.ALL_COUNTRIES, setCountries, 'countries');
+    const ukraine = countries.find((c) => c.value === 'Україна');
+    setSelectedCountry(ukraine || countries[0]);
+  }, []);
 
   return (
     <div
@@ -13,7 +26,7 @@ export const DeliveryInfoModal = () => {
     >
       <div className="delivery-modal__content">
         <ModalCloseBtn
-          clickHandler={setIsDeliveryModalOpen}
+          clickHandler={(e) => setIsDeliveryModalOpen(false)}
           extraClasses="delivery-modal__close"
         />
         <div className="delivery-modal__text-content">
@@ -23,13 +36,50 @@ export const DeliveryInfoModal = () => {
               <label htmlFor="country" className="delivery-modal__label">
                 Країна *
               </label>
-              <input
-                type="select"
-                defaultValue="Україна"
-                className="delivery-modal__input"
-                name="country"
-                id="country"
-              />
+              {countries.length && (
+                <Select
+                  options={countries.map((country) => ({
+                    value: country.title,
+                    label: country.title,
+                  }))}
+                  value={selectedCountry}
+                  onChange={setSelectedCountry}
+                  className="delivery-modal__input"
+                  defaultValue={selectedCountry}
+                  placeholder={selectedCountry}
+                  styles={{
+                    placeholder: (provided) => ({
+                      ...provided,
+                      color: 'black',
+                    }),
+                    option: (provided, state) => ({
+                      ...provided,
+                      backgroundColor: '#F2F3F6',
+                      border: 'none',
+                      paddingTop: 0,
+                      marginTop: '0px',
+                      fontWeight: 500,
+                      color: state.isSelected ? '#ff00c5' : 'black',
+                      borderBottom: '1px solid #E6E8EE',
+                      paddingTop: '10px',
+                      cursor: 'pointer',
+                    }),
+                    control: (base) => ({
+                      ...base,
+                      width: '100%',
+                      backgroundColor: '#F2F3F6',
+                      color: 'red',
+                      fontWeight: 500,
+                      height: '45px',
+                      border: 'none',
+                      borderRadius: '10px',
+                      paddingLeft: '5px',
+                    }),
+                  }}
+                />
+              )}
+              {(selectedCountry && console.log(selectedCountry)) ||
+                console.log('null')}
             </div>
             <div className="delivery-modal__field-group">
               <label htmlFor="city" className="delivery-modal__label">
